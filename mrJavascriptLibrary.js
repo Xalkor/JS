83ef4a91700f1fb823c7da6b259696d803e2defb
Xalkor <32653889+Xalkor@users.noreply.github.com>
2018-10-17 19:50:57 -0400

    Update mrJavascriptLibrary.js

--- mrJavascriptLibrary.js
+++ mrJavascriptLibrary.js
@@ -1,3 +1,8 @@
+let isStroke = true;
+let isFill = true;
+
+//converts 3 ints into a color string
+//or 1 int into a greyscale color string
 function color(r, g, b) {
   if (r && g && b) {
     return 'rgb(' + r + ',' + g + ',' + b + ')';
@@ -5,5 +10,43 @@ function color(r, g, b) {
     return 'rgb(' + r + ',' + r + ',' + r + ')';
   } else {
     return 'rgb(255,255,255)';
+  }
+}
+
+//draws a rectangle with a given color that covers the screen
+function background(r, g, b) {
+  ctx.fillStyle = color(r, g, b);
+  ctx.fillRect(0, 0, width, height);
+}
+
+//sets the fill color
+function fill(r, g, b) {
+  ctx.fillStyle = color(r, g, b);
+  isFill = true;
+}
+
+//sets the stroke color
+function stroke(r, g, b) {
+  ctx.strokeStyle = color(r, g, b);
+  isStroke = true;
+}
+
+//turns off fill
+function noFill() {
+  isFill = false;
+}
+
+//turns off stroke
+function noStroke() {
+  isStroke = false;
+}
+
+//draws a rectangle at a given x and y, with a given width and height
+function rect(x, y, width, height) {
+  if (isStroke) {
+    ctx.strokeRect(x, y, width, height);
+  }
+  if (isFill) {
+    ctx.fillRect(x, y, width, height);
   }
 }
