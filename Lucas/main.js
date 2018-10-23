6dcf7d6c6f87962256201bc5788bd6d332bd557f
Xalkor <32653889+Xalkor@users.noreply.github.com>
2018-10-23 10:13:54 -0400

    Update main.js

--- Lucas/main.js
+++ Lucas/main.js
@@ -1,5 +1,5 @@
 let h = 0;
-let word = "Lucas Pinto-Leight eats ass";
+let word;
 let can;
 
 function init() {
@@ -14,6 +14,16 @@ function init() {
     ctx = canvas.getContext('2d');
   }
 
+  word = decodeURI(getUrlVars()['daddy']);
+  if (word == "undefined") {
+    word = "Lucas Pinto-Leight";
+  }
+  word = word.substring(0, 25);
+  word += " eats ass";
+
+  width = word.length * 30 + 15;
+  canvas.width = width;
+
   ctx.font = "32px Consolas"
 
   clearTimeout(timer);
@@ -22,7 +32,7 @@ function init() {
 
 function draw(ctx) {
   ctx.fillStyle = "rgb(51,51,51)";
-  ctx.fillRect(0,0,width,height);
+  ctx.fillRect(0, 0, width, height);
   for (let i = 0; i < word.length; i++) {
     h += .75;
     ctx.fillStyle = "hsl(" + mod(Math.floor(h), 360) + ",100%,50%)";
@@ -31,7 +41,14 @@ function draw(ctx) {
 }
 
 function mouseMoved(event) {
-  console.log(event);
   can.style.top = event.clientY - height / 2 + "px";
   can.style.left = event.clientX - width / 2 + "px";
+}
+
+function getUrlVars() {
+  var vars = {};
+  var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m, key, value) {
+    vars[key] = value;
+  });
+  return vars;
 }
